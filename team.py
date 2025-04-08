f1e4cafe155c6c1175a8c2277f38dfc069c81999
chessesio <flavianchessesio@gmail.com>
2025-04-08 18:26:17 +0300

    Created team module and initialized class attributes

---
+++ team.py
@@ -0,0 +1,12 @@
+# module to hold and mutate team data
+
+class Team:
+    def __init__(self):
+        self.squad = {}
+        self.getSquad()
+
+    def addToSquad(self, player):
+        pass
+
+    def getSquad(self):
+        pass
